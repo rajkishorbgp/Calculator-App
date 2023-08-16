8ea3dc827ab774442f832fdd203fb960c96532af
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-08-16 17:59:20 +0530

    done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,14 +40,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    // calculator
-    implementation("com.faendir.rhino:rhino-android:1.5.2")
+    implementation ("net.objecthunter:exp4j:0.4.8")
 }
--- app/src/main/java/com/rajkishorbgp/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/rajkishorbgp/calculatorapp/MainActivity.kt
@@ -1,109 +1,103 @@
 package com.rajkishorbgp.calculatorapp
 
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.rajkishorbgp.calculatorapp.databinding.ActivityMainBinding
-
-
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var inputData: TextView
-    private lateinit var currentResult: TextView
+    private lateinit var binding: ActivityMainBinding
 
-    private var prevResult = ""
-    private var currentOperation = ""
     private var inputList = ""
-    private var currentResultValue = ""
-    private var equ =""
+    private var f = false
+    private lateinit var expression: Expression
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        inputData = findViewById(R.id.input_data)
-        currentResult = findViewById(R.id.currentResult)
 
+        setNumberClickListeners()
+        setOperationClickListeners()
+
+        binding.btnEquale.setOnClickListener {
+            onEqual()
+            binding.inputData.text=binding.currentResult.text.toString()
+            binding.currentResult.text= ""
+        }
+
+        binding.btnAllClear.setOnClickListener {
+            // Clear all inputs and reset the calculator
+            clearInputs()
+            binding.inputData.text=inputList
+        }
+        binding.btnBack.setOnClickListener {
+            if (inputList.isNotEmpty()) {
+                inputList = inputList.dropLast(1)
+                binding.inputData.text = inputList
+                val isOperator = "1234567890"
+                if (inputList.isNotEmpty() && inputList.last() in isOperator){
+                    onEqual()
+                }else if(inputList.isNotEmpty() && inputList.last() in isOperator){
+                    binding.currentResult.text=inputList
+                }else if(inputList.isEmpty()){
+                    binding.currentResult.text=inputList
+                }
+            }
+        }
+    }
+
+    private fun setNumberClickListeners() {
         val numberButtons = listOf(
-            binding.btn0, binding.btn1,binding.btn2, binding.btn3, binding.btn4, binding.btn5,
-            binding.btn6, binding.btn7,binding.btn8, binding.btn9, binding.btnPoint )
-
-        val operationButtons = listOf(
-            binding.btnAdd, binding.btnSub, binding.btnMultiply, binding.btnDivider, binding.btnModulo
+            binding.btn0, binding.btn1, binding.btn2, binding.btn3,
+            binding.btn4, binding.btn5, binding.btn6, binding.btn7,
+            binding.btn8, binding.btn9, binding.btnPoint
         )
 
         for (button in numberButtons) {
             button.setOnClickListener {
                 inputList += button.text.toString()
                 binding.inputData.text=inputList
-                binding.currentResult.text= calculateResult()
+                onEqual()
             }
         }
+    }
+
+    private fun setOperationClickListeners() {
+        val operationButtons = listOf(
+            binding.btnAdd, binding.btnSub, binding.btnMultiply,
+            binding.btnDivider, binding.btnModulo,binding.btnsqr
+        )
 
         for (button in operationButtons) {
             button.setOnClickListener {
                 inputList += button.text.toString()
                 binding.inputData.text=inputList
-                    //    binding.currentResult.text= calculateResult()
             }
-        }
-
-        binding.btnEquale.setOnClickListener {
-            calculateResult()
         }
     }
 
-    private fun calculateResult(): String{
-        val ope ="+-*/%"
-        var num1 =""
-        var num2 =""
-        var operation=""
-        var flog1 =true
-        val flog2 =true
-        var operation2 =""
-        var calculate=true
-        for ( char in inputList){
-            if (char in ope && num1.isEmpty() ){
-                num1 = char.toString()
-            }
-            if (flog1 && char !in ope){
-                num1 += char.toString()
-            }else if (char in ope && num2.isEmpty()){
-                operation = char.toString()
-                flog1=false
-            }else if (num1.isNotEmpty() && flog2 && char !in ope){
-                num2 += char.toString()
-            }
-//            else if (char in ope && operation.isNotEmpty()){
-//                operation2 = char.toString()
-//                flog2 = false
-//            }
-//            if (num1.isNotEmpty() && num2.isNotEmpty() && operation.isNotEmpty() && !flog1 && !flog2){
-//                num1 = calculate(num1.toDouble(),num2.toDouble(),operation).toString()
-//                num2 =""
-//                calculate= false
-//                operation=operation2
-//                operation2 = ""
-//            }
-        }
-//        if (calculate && operation.isNotEmpty()){
-//            num1 = calculate(num1.toDouble(),num2.toDouble(),operation).toString()
-//        }
-        Toast.makeText(this@MainActivity,"$num1 -> $num2",Toast.LENGTH_SHORT).show()
-        return num1
+
+
+    private fun clearInputs() {
+        inputList =""
+        binding.inputData.text=""
+        binding.currentResult.text=""
     }
 
-    private fun calculate(num1: Double, num2: Double, operation: String): Double {
-        val result =when(operation){
-            "+" -> num1+num2
-            "-" -> num1-num2
-            "*" -> num1*num2
-            "/" -> num1/num2
-            else -> 0.0
+    private fun onEqual() {
+        val txt = binding.inputData.text.toString()
+        val replaced = txt.replace("x", "*")
+        expression = ExpressionBuilder(replaced).build()
+        try {
+            val result = expression.evaluate()
+            binding.currentResult.text = result.toString()
+        } catch (ex: ArithmeticException) {
+            Log.e("EvaluateError", ex.toString())
+            binding.currentResult.text = "Error"
+
         }
-        return result
     }
 }
